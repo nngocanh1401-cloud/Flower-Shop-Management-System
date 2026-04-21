c0e3b8e629af09c3236ae282e82a907abb1d2fcd
nngocanh1401-cloud <nngocanh.1401@gmail.com>
2026-04-21 19:38:09 +0700

    update

--- T-SQL database.sql
+++ T-SQL database.sql
@@ -11,7 +11,6 @@ LoaiTrangThai varchar(20) not null,
 constraint CHK_LoaiTrangThai
 check (LoaiTrangThai in ('DonHang','PhieuNhapHang','PhieuTraHang'))
 );
-drop table TrangThai
 
 -- table NhaCungCap
 create table NhaCungCap (
