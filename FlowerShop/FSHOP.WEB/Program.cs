2c7900faa08591efcff84e3f58aecd335bb5be01
lee hyn <lsx2245@gmail.com>
2026-05-18 17:48:00 +0700

    fix cors policy

--- FlowerShop/FSHOP.WEB/Program.cs
+++ FlowerShop/FSHOP.WEB/Program.cs
@@ -105,8 +105,9 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAngular", policy =>
     {
-        policy.WithOrigins("http://localhost:4200") 
-              .AllowAnyMethod();
+        policy.WithOrigins("http://localhost:4200")
+              .AllowAnyMethod()
+              .AllowAnyHeader();
     });
 });
 var app = builder.Build();
