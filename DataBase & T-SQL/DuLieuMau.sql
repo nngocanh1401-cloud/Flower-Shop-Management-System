163bd791a87a7199d94372d1c7af5a701fb269db
nngocanh1401-cloud <nngocanh.1401@gmail.com>
2026-05-18 18:08:01 +0700

    fix

--- DataBase & T-SQL/DuLieuMau.sql
+++ DataBase & T-SQL/DuLieuMau.sql
@@ -7,12 +7,7 @@ INSERT INTO TrangThai VALUES
 (3, N'Hoàn thành', N'Giao thành công', 'DonHang'),
 (4, N'Đã hủy',   N'Đơn bị hủy',     'DonHang'),
 (5, N'Chờ duyệt', N'Phiếu nhập chờ duyệt',  'PhieuNhapHang'),
-(6, N'Đã nhập', N'Đã nhập hàng',    'PhieuNhapHang'),
-(7, N'Chờ xử lý',  N'Phiếu trả chờ xử lý',  'PhieuTraHang'),
-(8, N'Đã trả',  N'Đã trả hàng cho NCC',     'PhieuTraHang');
-
--- KIỂM TRA
-SELECT * FROM TrangThai ORDER BY MaTrangThai;
+(6, N'Đã nhập', N'Đã nhập hàng',    'PhieuNhapHang');
 
 -- NhaCungCap
 INSERT INTO NhaCungCap VALUES
